a34d87cf1721ad7b717ca6f1898d6955c9c2ad90
Cleret Francis <franciscleret@sfr.fr>
2023-03-10 07:52:48 +0100

    Add level cap & fix merge of some items

--- Items/ModGlobalItem.cs
+++ Items/ModGlobalItem.cs
@@ -8,141 +8,161 @@ using Terraria.ModLoader.IO;
 
 namespace WeaponFusion.Items
 {
-    public class ModGlobalItem : GlobalItem
-    {
-        public override bool InstancePerEntity => true;
-        public int level = 1;
+	public class ModGlobalItem : GlobalItem
+	{
+		public override bool InstancePerEntity => true;
+		public int level = 1;
 
-        public override void SetDefaults(Item item)
-        {
-        }
+		#region Override Methods
 
-        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+		{
+			if (CheckCompatibility(item) != CompatibleType.None)
+			{
+				TooltipLine line = new(Mod, "", $"Level: {level}")
+				{
+					OverrideColor = Color.Cyan
+				};
+				tooltips.Add(line);
+			}
+		}
+
+		public override bool CanRightClick(Item item)
+		{
+			return CheckCompatibility(item) != CompatibleType.None || base.CanRightClick(item);
+		}
+
+		public override bool ConsumeItem(Item item, Player player)
+		{
+			return CheckCompatibility(item) == CompatibleType.None && base.ConsumeItem(item, player);
+		}
+
+		public override void RightClick(Item item, Player player)
         {
-            if (CheckCompatibility(item) != CompatibleType.None)
+            bool isUnderMaxLevel = GetLevel(item) < (int)GetConfigValue(nameof(WeaponFusionConfig.MaxLevel));
+            if (isUnderMaxLevel && GetCombustibleItem(item, player) is Item combustibleItem)
+			{
+				SetLevel(item, GetLevel(item) + 1);
+				combustibleItem.TurnToAir();
+			}
+			else
+			{
+				base.RightClick(item, player);
+			}
+		}
+
+		public override void PostReforge(Item item)
+		{
+			SetLevel(item, GetLevel(item));
+		}
+
+		public override void LoadData(Item item, TagCompound tag)
+		{
+			SetLevel(item, tag.Get<int>(nameof(level)));
+		}
+
+		public override void SaveData(Item item, TagCompound tag)
+		{
+			tag.Set(nameof(level), GetLevel(item));
+		}
+
+		#endregion
+
+		#region Custom Methods
+
+		public static void SetLevel(Item item, int value)
+		{
+			Item defaultItem = new();
+			defaultItem.SetDefaults(item.netID);
+
+			item.GetGlobalItem<ModGlobalItem>().level = value;
+
+			switch (CheckCompatibility(item))
+			{
+				case CompatibleType.Both:
+					item.damage = GetValueByLevel(defaultItem.damage, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDamage)));
+					item.defense = GetValueByLevel(defaultItem.defense, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDefense)));
+					break;
+				case CompatibleType.Damage:
+					item.damage = GetValueByLevel(defaultItem.damage, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDamage)));
+					break;
+				case CompatibleType.Defense:
+					item.defense = GetValueByLevel(defaultItem.defense, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDefense)));
+					break;
+				default:
+					break;
+			}
+		}
+
+		public static int GetLevel(Item item)
+		{
+			return item.GetGlobalItem<ModGlobalItem>().level;
+		}
+
+		private static object GetConfigValue(string valueName)
+		{
+			return WeaponFusionConfig.Current.GetType().GetField(valueName).GetValue(WeaponFusionConfig.Current);
+		}
+
+		private static int GetValueByLevel(int initialValue, int level, float multiplier)
+		{
+			int returnValue = (int)(initialValue + ((level - 1) * initialValue * multiplier));
+			return returnValue < 0 ? 0 : returnValue;
+		}
+
+		private static Item GetCombustibleItem(Item item, Player player)
+		{
+			if (CheckCompatibility(item) != CompatibleType.None)
+			{
+				foreach (Item itemSlot in player.inventory)
+				{
+					if (!itemSlot.Equals(player.HeldItem)
+						&& !itemSlot.Equals(item)
+						&& itemSlot.netID == item.netID
+						&& GetLevel(itemSlot) == GetLevel(item))
+					{
+						return itemSlot;
+					}
+				}
+			}
+			return null;
+		}
+
+		private static CompatibleType CheckCompatibility(Item item)
+		{
+			Item defaultItem = new();
+			defaultItem.SetDefaults(item.netID);
+
+			CompatibleType type = CompatibleType.None;
+
+			// IsCandidateForReforge ?
+
+			bool isBlacklisted = ((List<ItemDefinition>)GetConfigValue(nameof(WeaponFusionConfig.Blacklist))).Any(e => e.Type == item.netID);
+
+            if (!isBlacklisted && item.stack == 1)
             {
-                TooltipLine line = new(Mod, "", $"Level: {level}")
-                {
-                    OverrideColor = Color.Cyan
-                };
-                tooltips.Add(line);
-            }
-        }
+                if (defaultItem.damage > 0 && defaultItem.IsCandidateForReforge)
+				{
+					type = CompatibleType.Damage;
+				}
+				if (defaultItem.defense > 0)
+				{
+					type = type == CompatibleType.Damage
+						? CompatibleType.Both : CompatibleType.Defense;
+				}
+			}
 
-        public override bool CanRightClick(Item item)
-        {
-            return CheckCompatibility(item) != CompatibleType.None || base.CanRightClick(item);
-        }
+			return type;
+		}
 
-        public override bool ConsumeItem(Item item, Player player)
-        {
-            return CheckCompatibility(item) == CompatibleType.None && base.ConsumeItem(item, player);
-        }
+		#endregion
+	}
 
-        public override void RightClick(Item item, Player player)
-        {
-            if (CheckCompatibility(item) != CompatibleType.None)
-            {
-                foreach (Item itemSlot in player.inventory)
-                {
-                    if (!itemSlot.Equals(player.HeldItem)
-                        && !itemSlot.Equals(item)
-                        && itemSlot.netID == item.netID
-                        && GetLevel(itemSlot) == GetLevel(item))
-                    {
-                        SetLevel(item, GetLevel(item) + 1);
-                        itemSlot.TurnToAir();
-                        return;
-                    }
-                }
-            }
-        }
-
-        public override void PostReforge(Item item)
-        {
-            SetLevel(item, GetLevel(item));
-        }
-
-        public override void LoadData(Item item, TagCompound tag)
-        {
-            SetLevel(item, tag.Get<int>(nameof(level)));
-        }
-
-        public override void SaveData(Item item, TagCompound tag)
-        {
-            tag.Set(nameof(level), GetLevel(item));
-        }
-
-        public static void SetLevel(Item item, int value)
-        {
-            Item defaultItem = new();
-            defaultItem.SetDefaults(item.netID);
-
-            item.GetGlobalItem<ModGlobalItem>().level = value;
-
-            switch (CheckCompatibility(item))
-            {
-                case CompatibleType.Both:
-                    item.damage = GetValueByLevel(defaultItem.damage, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDamage)));
-                    item.defense = GetValueByLevel(defaultItem.defense, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDefense)));
-                    break;
-                case CompatibleType.Damage:
-                    item.damage = GetValueByLevel(defaultItem.damage, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDamage)));
-                    break;
-                case CompatibleType.Defense:
-                    item.defense = GetValueByLevel(defaultItem.defense, value, (float)GetConfigValue(nameof(WeaponFusionConfig.MultDefense)));
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public static int GetLevel(Item item)
-        {
-            return item.GetGlobalItem<ModGlobalItem>().level;
-        }
-
-        private static object GetConfigValue(string valueName)
-        {
-            return WeaponFusionConfig.Current.GetType().GetField(valueName).GetValue(WeaponFusionConfig.Current);
-        }
-
-        private static int GetValueByLevel(int initialValue, int level, float multiplier)
-        {
-            int returnValue = (int)(initialValue + ((level - 1) * initialValue * multiplier));
-            return returnValue < 0 ? 0 : returnValue;
-        }
-
-        private static CompatibleType CheckCompatibility(Item item)
-        {
-            Item defaultItem = new();
-            defaultItem.SetDefaults(item.netID);
-
-            CompatibleType type = CompatibleType.None;
-
-            bool isBlacklisted = ((List<ItemDefinition>)GetConfigValue(nameof(WeaponFusionConfig.Blacklist))).Any(e => e.Type == item.netID);
-            if (!isBlacklisted)
-            {
-                if (defaultItem.damage > 0)
-                {
-                    type = CompatibleType.Damage;
-                }
-                if (defaultItem.defense > 0)
-                {
-                    type = type == CompatibleType.Damage
-                        ? CompatibleType.Both : CompatibleType.Defense;
-                }
-            }
-
-            return type;
-        }
-    }
-
-    public enum CompatibleType
-    {
-        None,
-        Both,
-        Damage,
-        Defense
-    }
+	public enum CompatibleType
+	{
+		None,
+		Both,
+		Damage,
+		Defense
+	}
 }
--- WeaponFusionConfig.cs
+++ WeaponFusionConfig.cs
@@ -7,37 +7,47 @@ using Terraria.ModLoader.Config;
 
 namespace WeaponFusion
 {
-    public class WeaponFusionConfig : ModConfig
-    {
-        public override ConfigScope Mode => ConfigScope.ServerSide;
+	public class WeaponFusionConfig : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ServerSide;
 
-        public static WeaponFusionConfig Current => ModContent.GetInstance<WeaponFusionConfig>();
+		public static WeaponFusionConfig Current => ModContent.GetInstance<WeaponFusionConfig>();
 
-        [Header("Weapon Fusion")]
+		[Header("Weapon Fusion")]
 
-        [Label("Damage multiplier")]
-        [DefaultValue(0.5f)]
-        [Range(0.0f, 1.0f)]
-        [ReloadRequired]
-        public float MultDamage;
+		[Label("Damage multiplier")]
+		[DefaultValue(0.5f)]
+		[Range(0.0f, 1.0f)]
+		[ReloadRequired]
+		public float MultDamage;
 
-        [Label("Defense multiplier")]
-        [DefaultValue(0.5f)]
-        [Range(0.0f, 1.0f)]
-        [ReloadRequired]
-        public float MultDefense;
+		[Label("Defense multiplier")]
+		[DefaultValue(0.5f)]
+		[Range(0.0f, 1.0f)]
+		[ReloadRequired]
+		public float MultDefense;
 
-        [Label("Override prefix")]
-        [Tooltip("Damage and defense bonus from prefix are overrides. Not configurable for now. [WIP]")]
-        [JsonProperty]
-        public bool OverridePrefix { get; } = true;
+		[Label("Max level")]
+		[DefaultValue(999)]
+		[Range(0, 999)]
+		public int MaxLevel;
 
-        [Label("Blacklist")]
-        public List<ItemDefinition> Blacklist = new()
-        {
-            { new ItemDefinition(24) }, // Wooden Sword
-            { new ItemDefinition(284) }, // Wooden Boomerang
-            { new ItemDefinition(55) } // Enchanted Boomerang
-        };
-    }
+		[Label("Allow commands on multiplayer for non-host players")]
+		[DefaultValue(false)]
+		[JsonProperty]
+		public bool EnabledCommands { get; } = false;
+
+		[Label("Override prefix")]
+		[Tooltip("Damage and defense bonus from prefix are overrides. Not configurable for now. [WIP]")]
+		[JsonProperty]
+		public bool OverridePrefix { get; } = true;
+
+		[Label("Blacklist")]
+		public List<ItemDefinition> Blacklist = new()
+		{
+			{ new ItemDefinition(24) }, // Wooden Sword
+			{ new ItemDefinition(284) }, // Wooden Boomerang
+			{ new ItemDefinition(55) } // Enchanted Boomerang
+		};
+	}
 }
