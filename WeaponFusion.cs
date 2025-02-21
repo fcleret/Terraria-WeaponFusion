ba9baa013faa3e13045aadcf7592bbbcea8840b9
Francis CLERET <fcleret@deltamu.com>
2025-02-21 15:57:35 +0100

    Handle config event without the lag issue

--- WeaponFusion.cs
+++ WeaponFusion.cs
@@ -1,8 +1,36 @@
 using Terraria.ModLoader;
+using WeaponFusion.Utils;
 
 namespace WeaponFusion
 {
 	public class WeaponFusion : Mod
 	{
+		public override void Load()
+		{
+			WeaponFusionConfig.ConfigurationChanged += ReloadItemsStats;
+		}
+
+		public override void Unload()
+		{
+			WeaponFusionConfig.ConfigurationChanged -= ReloadItemsStats;
+		}
+
+		private void ReloadItemsStats()
+		{
+			foreach (Player player in Main.player)
+			{
+				if (player?.inventory == null) 
+					continue;
+
+				foreach (Item item in player.inventory)
+				{
+					if (MergeHelper.IsCompatible(item))
+					{
+						MergeHelper.SetLevel(item, MergeHelper.GetLevel(item));
+						item.RebuildTooltip();
+					}
+				}
+			}
+		}
 	}
 }
